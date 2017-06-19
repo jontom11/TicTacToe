3f45af76161c8464df8a75be9d9579d23f65413b
Jonathan Tom <Jonathan.Y.Tom1@gmail.com>
2017-06-19 14:58:24 -0700

    initial repo

---
+++ TicTacToe.js
@@ -0,0 +1,18 @@
+// tic tac toe
+
+var Board = function () {
+	this.board = [[null, null, null],[null, null, null],[null, null, null]];
+	
+  
+}
+
+// start with empty board
+// x always starts (first person)
+  // prompt x to select a space
+    // click a square which will change the square to an X
+    // run the win function to determine a 3 in a row
+  // prompt o to select a space
+    // click a square which will change the square to an o
+    // run the win function to determine a 3 in a row
+
+
